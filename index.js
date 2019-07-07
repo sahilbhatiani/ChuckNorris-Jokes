95d3936ef96682ad7e550ea1da54d4f64a9bb655
Sahil <sahilbhatiani28@gmail.com>
2019-07-07 20:14:24 +1200

    Add all files

---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = async function (context, myTimer) {
+    
+
+    const accountSid ="AC0b5a73ac23f11444b71d3a5f583e49c8"
+    const authToken ="78335ee9d677caea7037437b9653529a"
+
+    const client = require('twilio')(accountSid, authToken)
+
+    var timeStamp = new Date().toISOString();
+
+    const https = require('https');
+
+
+    let message = '';
+
+    //Using the Chuck Norris jokes API
+    https.get('https://api.chucknorris.io/jokes/random', (resp) => {
+    let data = '';
+   
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        x = (JSON.parse(data).value);
+        context.log(x);
+         if (myTimer.IsPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    client.messages.create({
+        to:"+64212603729",
+        from:'+12674607418',
+        body:message
+    })  
+    });
+
+    })
+
+
+    
+   
+};
+
